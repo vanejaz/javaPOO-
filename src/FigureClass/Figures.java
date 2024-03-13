4f4fa83de68a3e76eb803fa5e2c94426c55827b8
Vane <jaz.lsv.79@gmail.com>
2024-03-12 23:38:28 -0600

    Se agregan más Ejercicios de POO

---
+++ src/FigureClass/Figures.java
@@ -0,0 +1,4 @@
+package FigureClass;
+
+public class Figures {
+}
---
+++ src/FigureClass/SuperHero.java
@@ -0,0 +1,12 @@
+public class SuperHero {
+    private String name;
+    private String description;
+    private Boolean capa;
+
+    public SuperHero(String name) {
+        this.name = name;
+        this.description="";
+        this.capa=false;
+    }
+}
+
